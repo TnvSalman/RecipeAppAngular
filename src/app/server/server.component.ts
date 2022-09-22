55bf2d70b4c6ed332cc841409d16b2d2a36b02c8
Salman Tanirverdiyev <tnvsmn@gmail.com>
2022-09-22 17:51:16 +0200

    ngClass and ngStyle built in directives added

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,15 +2,28 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styles:[
+      `
+      .online{
+        color:'white';
+      }
+      
+      `
+    ]
 })
 export class ServerComponent {
     serverId:number = Math.floor(Math.random() * 10);
-    serverStatus:string = Math.floor(Math.random() * 10) > 5 ? 'offline': 'online';
+    serverStatus:string = "offline";
     serverColor:string = this.serverStatus === 'online'? "text-primary":"text-danger";
-
+    constructor(){
+      this.serverStatus = Math.random() > 0.5 ? 'online' : ' offline';
+    }
     getServerStatus(){
       return this.serverStatus;
+    }
+    getColor(){
+      return this.serverStatus === 'online' ? 'green' : 'red';
     }
 
 }
